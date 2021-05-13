89b1e39c51b292b18002282bc5af33c5f96a9704
Jerome Soumagne <jsoumagne@hdfgroup.org>
2021-05-13 13:51:59 -0500

    CMake: no need to link against ucs and uct libraries

--- src/na/CMake/FindUCX.cmake
+++ src/na/CMake/FindUCX.cmake
@@ -13,21 +13,22 @@ find_path(UCX_INCLUDE_DIR ucp/api/ucp.h
 find_library(UCP_LIBRARY NAMES ucp
   HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
 
-find_library(UCT_LIBRARY NAMES uct
-  HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
+#find_library(UCT_LIBRARY NAMES uct
+#  HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
 
-find_library(UCS_LIBRARY NAMES ucs
-  HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
+#find_library(UCS_LIBRARY NAMES ucs
+#  HINTS ${PC_UCX_LIBDIR} ${PC_UCX_LIBRARY_DIRS})
 
 set(UCX_INCLUDE_DIRS ${UCX_INCLUDE_DIR})
-set(UCX_LIBRARIES ${UCP_LIBRARY} ${UCT_LIBRARY} ${UCS_LIBRARY})
+set(UCX_LIBRARIES ${UCP_LIBRARY})
+# ${UCT_LIBRARY} ${UCS_LIBRARY}
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set UCX_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(UCX DEFAULT_MSG
                                   UCX_INCLUDE_DIR
-				  UCP_LIBRARY UCT_LIBRARY UCS_LIBRARY)
+                                  UCP_LIBRARY) # UCT_LIBRARY UCS_LIBRARY
 
-mark_as_advanced(UCX_INCLUDE_DIR UCP_LIBRARY UCT_LIBRARY UCS_LIBRARY)
+mark_as_advanced(UCX_INCLUDE_DIR UCP_LIBRARY) # UCT_LIBRARY UCS_LIBRARY
 
